c4555a72ceeabfb7a053f3d94e4ba869645364c4
saebyeolp <saebyeolpak@gmail.com>
2021-04-02 10:48:05 -0700

    scroll-animation

--- projects/scroll-animation/js/main.js
+++ projects/scroll-animation/js/main.js
@@ -5,10 +5,10 @@ window.addEventListener('scroll', checkBoxes)
 checkBoxes()
 
 function checkBoxes() {
-    const triggerBottom = window.innerHeight / 5 * 4
+    const triggerBottom = window.innerHeight / 5 * 4  //브라우저의 창의 높이
 
     boxes.forEach(box => {
-        const boxTop = box.getBoundingClientRect().top
+        const boxTop = box.getBoundingClientRect().top //사용자의 위치값 구하기
 
         if(boxTop < triggerBottom) {
             box.classList.add('show')
@@ -17,3 +17,4 @@ function checkBoxes() {
         }
     })
 }
+

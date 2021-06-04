e36232cddd494a087d7cd67f99e07364af55fdc7
saebyeolp <saebyeolpak@gmail.com>
2021-06-04 09:48:00 -0700

    sticky nav bar

--- projects/theme-clock/js/main.js
+++ projects/theme-clock/js/main.js
@@ -32,7 +32,7 @@ const setTime = () => {
     const seconds = time.getSeconds()
     const ampm = hours >= 12 ? 'PM' : 'AM'
 
-    hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(hoursForClock, 0, 11.5, 0, 360)}deg)`
+    hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(hoursForClock, 0, 12, 0, 360)}deg)`
     minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(minutes, 0, 59, 0, 360)}deg)`
     secondEl.style.transform = `translate(-50%, -100%) rotate(${scale(seconds, 0, 59, 0, 360)}deg)`
 

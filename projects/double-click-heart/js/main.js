707e917bcffb5a72da8e663ad1fd9f620aa59531
saebyeolp <saebyeolpak@gmail.com>
2021-06-15 09:49:30 -0700

    double click heart

---
+++ projects/double-click-heart/js/main.js
@@ -0,0 +1,44 @@
+const loveMe = document.querySelector('.loveMe')
+const times = document.querySelector('#times')
+
+let clickTime = 0
+let timesClicked = 0
+
+loveMe.addEventListener('click', (e) => {
+    if(clickTime === 0) {
+        clickTime = new Date().getTime()
+    } else {
+        if((new Date().getTime() - clickTime) < 800) {
+            createHeart(e)
+            clickTime = 0
+        } else {
+            clickTime = new Date().getTime()
+        }
+    }
+})
+
+const createHeart = (e) => {
+    const heart = document.createElement('i')
+    heart.classList.add('fas')
+    heart.classList.add('fa-heart')
+
+    const x = e.clientX
+    const y = e.clientY
+    //이벤트가 발생한 곳의 좌표를 제공함
+
+    const leftOffset = e.target.offsetLeft
+    const topOffset = e.target.offsetTop
+    //현재 포지션을 리턴해줌
+
+    const xInside = x - leftOffset
+    const yInside = y - topOffset
+
+    heart.style.top = `${yInside}px`
+    heart.style.left = `${xInside}px`
+
+    loveMe.appendChild(heart)
+
+    times.innerHTML = ++timesClicked
+
+    setTimeout(() => heart.remove(), 1000)
+}

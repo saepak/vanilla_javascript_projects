02d231d83d51323b981436cea0ce9ea9f433f841
saebyeolp <saebyeolpak@gmail.com>
2021-07-30 10:38:09 -0700

    mobile navigation

--- projects/checkbox/js/main.js
+++ projects/checkbox/js/main.js
@@ -5,10 +5,10 @@ const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]')
 let lastChecked
 
 function handleCheck(e) {
-  // 시프트키가 다운인지 체크
 
   let inBetween = false
   if (e.shiftKey && this.checked) {
+        // 시프트키 눌렀고 체크가 되어있을때
 
     checkboxes.forEach(checkbox => {
       console.log(checkbox)

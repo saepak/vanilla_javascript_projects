bd0af9b01a1a9094e6f4e7d7ec3c0e08335dceeb
saebyeolp <saebyeolpak@gmail.com>
2021-08-30 09:58:38 -0700

    slide in scroll

--- projects/slide-in-scroll/js/main.js
+++ projects/slide-in-scroll/js/main.js
@@ -23,7 +23,16 @@ const checkSlide = (e) => {
     sliderImages.forEach(sliderImage => {
         const slideInAt = (window.scrollY + window.innerHeight) - sliderImage.height / 2 
         // /2안하면 각 이미지가 윈도우 끝에 왔을때 높이를 구함 나누기를 해서 이미지 절반쯤 왔을때의 높이를 구함
-        console.log(slideInAt)
+        const imageBottom = sliderImage.offsetTop + sliderImage.height
+        //offsetTop은 각 이미지 상단이 윈도우 상단이랑 얼마나 떨어져 있는지 보여줌
+        const isHalfShown = slideInAt > sliderImage.offsetTop
+        const isNotScrolledPast = window.scrollY < imageBottom
+        
+        if(isHalfShown && isNotScrolledPast) {
+            sliderImage.classList.add('active')
+        } else {
+            sliderImage.classList.remove('active')
+        }
     })
 }
 

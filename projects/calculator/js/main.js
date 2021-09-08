2b3cde5bb0e442b1ff9dd1ee9c35b054eca757de
saebyeolp <saebyeolpak@gmail.com>
2021-09-08 10:28:22 -0700

    calculator

--- projects/calculator/js/main.js
+++ projects/calculator/js/main.js
@@ -15,15 +15,16 @@ class Calculator {
     }
 
     delete(){
-
+        this.currentOperand = this.currentOperand.toString().slice(0, -1)
     }
+    //클릭할때 끝 숫자부터 하나씩 지워줌
 
     appendNumber(number){
         if (number === '.' && this.currentOperand.includes('.')) return
         this.currentOperand = this.currentOperand.toString() + number.toString()
-        //.을 많이 클릭해도 하나만 나오도록 
-        //+가 있어도 계산되지 않게 toString으로 문자로 바꿈 
     }
+    //.을 많이 클릭해도 하나만 나오도록 
+    //+가 있어도 계산되지 않게 toString으로 문자로 바꿈 
     
     chooseOperation(operation){
         if(this.currentOperand === '') return
@@ -37,12 +38,61 @@ class Calculator {
     }
 
     compute(){
-
+        let computation
+        const prev = parseFloat(this.previousOperand)
+        const current = parseFloat(this.currentOperand)
+        if(isNaN(prev) || isNaN(current)) return
+        switch(this.operation){
+            case '+':
+                computation = prev + current
+                break
+            case '-':
+                computation = prev - current
+                break
+            case '*':
+                computation = prev * current
+                break
+            case '÷':
+                computation = prev / current
+            break
+            default:
+                return
+        }
+        this.currentOperand = computation
+        this.operation = undefined
+        this.previousOperand = ''
     }
 
+    getDisplayNumber(number){
+        const stringNumber = number.toString()
+        const integerDigits = parseFloat(stringNumber.split('.')[0])
+        const decimalDigits = stringNumber.split('.')[1]
+        let integerDisplay
+        if(isNaN(integerDigits)){
+            integerDisplay = ''
+        } else {
+            integerDisplay = integerDigits.toLocaleString('en', {
+                maximumFractionDigits: 0
+            })
+        }
+        if(decimalDigits != null){
+            return `${integerDisplay}. ${decimalDigits}`
+        } else {
+            return integerDisplay
+        }
+    }
+    //숫자에 ,추가. 스트링인 숫자를 숫자로 바꿔주고 ,추가
+    //소수에 대한 표시도 추가
+
     updateDisplay(){
-        this.currentOperandTextElement.innerText = this.currentOperand
-        this.previousOperandTextElement.innerText = this.previousOperand
+        this.currentOperandTextElement.innerText = this.getDisplayNumber(this.currentOperand)
+        if(this.operation != null){
+            this.previousOperandTextElement.innerText = 
+                `${this.previousOperand} ${this.operation}`
+        }else {
+            this.previousOperandTextElement.innerText = ''
+        }
+        //계산을 마친뒤에는 상단 숫자가 없어짐
     }
 }
 
@@ -66,11 +116,24 @@ numberButtons.forEach(button => {
     })
 })
 
-
-
 operationButtons.forEach(button => {
     button.addEventListener('click', () => {
       calculator.chooseOperation(button.innerText)
       calculator.updateDisplay()
     })
+})
+
+equalsButton.addEventListener('click', button => {
+    calculator.compute()
+    calculator.updateDisplay()
+})
+
+allClearButton.addEventListener('click', button => {
+    calculator.clear()
+    calculator.updateDisplay()
+})
+
+deleteButton.addEventListener('click', button => {
+    calculator.delete()
+    calculator.updateDisplay()
 })

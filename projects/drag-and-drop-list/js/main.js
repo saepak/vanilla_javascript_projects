4f11d0bf38f3c3804de7b101fd9f0826767b38bb
saebyeolp <saebyeolpak@gmail.com>
2021-10-05 10:48:53 -0700

    drag and drop list

---
+++ projects/drag-and-drop-list/js/main.js
@@ -0,0 +1,14 @@
+
+
+const topAlbumSales = [
+    'Folklore, Taylor Swift',
+    'After Hours, The Weeknd',
+    'Map of the Soul: 7, BTS',
+    'Legends Never Die, Juice Wrld',
+    'Fine Line, Harry Styles',
+    'Chromatica, Lady Gaga',
+    'Manic, Halsey',
+    'Evermore, Taylor Swift',
+    'Music to Be Murdered By, Eminem',
+    'Here and Now, Kenny Chesney'
+]

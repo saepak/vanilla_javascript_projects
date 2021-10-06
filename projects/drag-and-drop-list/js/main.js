c2244d4247b40c2c50bda7f5ead67578390eb319
saebyeolp <saebyeolpak@gmail.com>
2021-10-06 12:18:55 -0700

    drag and drop list

--- projects/drag-and-drop-list/js/main.js
+++ projects/drag-and-drop-list/js/main.js
@@ -1,3 +1,5 @@
+const draggable_list = document.getElementById('draggable-list')
+const check = document.getElementById('check')
 
 
 const topAlbumSales = [
@@ -12,3 +14,32 @@ const topAlbumSales = [
     'Music to Be Murdered By, Eminem',
     'Here and Now, Kenny Chesney'
 ]
+
+
+// Store listitems
+const listItems = []
+let dragStartIndex
+
+createList()
+
+// Insert list itmes to DOM
+function createList() {
+    [...topAlbumSales] //원래 어레이를 바꾸지 않고 사용
+        .forEach((person, index) => {
+            const listItem = document.createElement('li')   
+
+            listItem.setAttribute('data-index', index)
+
+            listItem.innerHTML = `
+                <span class="number>${index+1}</span>
+                <div class="draggable" draggable="true">
+                    <p class="person-name">${person}</p>
+                    <i class="fas fa-grip-lines"></i>
+                </div>
+            `
+
+            listItems.push(listItem)
+
+            draggable_list.appendChild(listItem)
+        })
+}
